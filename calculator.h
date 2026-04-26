1226d064f58e3eeccea154f77a792c0a64774a77
Sebastian <sebastianfarkash@gmail.com>
2026-04-26 20:40:59 +0300

    Add Calculator class with basic arithmetic methods

---
+++ calculator.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    double add(double a, double b);      // Марчук
+    double subtract(double a, double b); // Лихенко
+};
+
+#endif
